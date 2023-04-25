0f2b84eb193a7918849ca35337649bbdbdc39fc2
GyoHeon Lee <gh.lee@muzlive.com>
2023-04-25 15:57:40 +0900

    Course: 6.0.1

--- script/index.ts
+++ script/index.ts
@@ -1,6 +1,8 @@
 import page from "./page";
 import router from "./router";
 
+const NAV_BTN_SELECTOR = "button[data-navigate]";
+
 const container = document.querySelector("main") as HTMLElement;
 
 const pages = page(container);
@@ -8,3 +10,12 @@ const pages = page(container);
 const route = router();
 
 route.addRouter("#/", pages.home).addRouter("#/list", pages.list).setNotFound(pages.notFound).start();
+
+document.body.addEventListener("click", (e: Event) => {
+  const target = e.target as HTMLElement;
+
+  if (target.matches(NAV_BTN_SELECTOR)) {
+    const path = target.dataset.navigate as string;
+    route.navigate(path);
+  }
+});

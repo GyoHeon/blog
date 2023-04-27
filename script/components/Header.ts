4f9a12e95686920880096ef3db4467d654372811
GyoHeonLee <ghl12078@gmail.com>
2023-04-28 02:53:01 +0900

    Chore: Add favicon

--- script/components/Header.ts
+++ script/components/Header.ts
@@ -8,8 +8,12 @@ class AppHeader extends HTMLElement {
       this.innerHTML = `
         <header>
           <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
+            <li><a data-navigation href="/">Go To Home</a></li>
+            <li><a data-navigation href="/list">Go To Lits</a></li>
+            <li><a data-navigation href="/list/1">Go To Detail with Id 1</a></li>
+            <li><a data-navigation href="/list/2">Go To Detail with Id 2</a></li>
+            <li><a data-navigation href="/list/1/2">Go To Another Detail</a></li>
+            <li><a data-navigation href="/dummy">Dummy Page</a></li>
           </ul>
         </header>
         `;

edaa7fa11ac30678d7f961a4984fe4d35b875187
GyoHeonLee <ghl12078@gmail.com>
2023-04-29 02:10:21 +0900

    Style: Edit global styles

--- script/components/Header.ts
+++ script/components/Header.ts
@@ -2,13 +2,22 @@ const template = document.createElement("template");
 template.innerHTML = `
   <style>
     header {
+      max-width: 700px;
+      height: 100px;
+      margin: 20px auto;
+      border-radius: 20px;
+      background: #403a36;
+      box-shadow:  20px -20px 60px #36312e,
+             -20px 20px 60px #4a433e;
+    }
+    img {
+      width: 50px;
       height: 50px;
-      background-color: #333;
     }
     ul {
-      display: flex;
-      justify-content: space-around;
-      align-items: center;
+      display: grid;
+      grid-auto-flow: column;
+      place-items: center;
       height: 100%;
     }
     a {
@@ -22,6 +31,11 @@ template.innerHTML = `
 
   <header>
     <ul>
+      <li>
+        <h1>
+          <image src='/public/image/gear.png' alt='main image of blog Developer GyoHeon' />
+        </h1>
+      </li>
       <li><a data-navigation href="/">Home</a></li>
       <li><a data-navigation href="/posts">Posts</a></li>
       <li><a data-navigation href="/resume">Resume</a></li>

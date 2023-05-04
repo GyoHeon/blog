372d183929af49d40bf3622e0f94e2dee607590e
GyoHeonLee <ghl12078@gmail.com>
2023-05-05 03:32:04 +0900

    Feat: Edit footer

--- script/components/Footer.ts
+++ script/components/Footer.ts
@@ -5,14 +5,29 @@ template.innerHTML = `
       margin-top: auto;
       box-shadow: var(--neumo);
     }
-    footer {
-      height: 50px;
+    .main--footer {
+      height: 10rem;
+      padding: 1rem;
+    }
+    .main--footer h2 {
+      font-size: 1.5rem;
+      margin-bottom: 1rem;
+    }
+    .main--footer p {
+      line-height: 1.3rem;
     }
   </style>
 
-  <footer>
+  <footer class="main--footer">
     <h2>© 2023 GyoHeon</h2>
-    <p>This blog is made by pure TypeScript. All of Codes is </p>
+    <p>
+    This blog is created without any frameworks,
+    </br>
+    using pure TypeScript, and will focus on sharing
+    </br>
+    tech-related articles primarily about frontend development.
+    </br>
+    I hope you enjoy your time here!</p>
   </footer>
   `;
 

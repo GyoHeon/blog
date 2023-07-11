31f4a1bddf2efbc006939cce91d9a44480794199
GyoHeonLee <ghl12078@gmail.com>
2023-07-12 01:15:44 +0900

    Feat: Init RESUME page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,3 +1,18 @@
+import { RESUME_HEAD } from "@/constants/resume";
+import Image from "next/image";
+
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>;
+  return (
+    <main className="flex flex-col gap-12 w-full">
+      <section>
+        <h2 className="text-6xl font-bold">{RESUME_HEAD.name}</h2>
+        <div>
+          <Image src={RESUME_HEAD.image} width={300} height={300} alt={RESUME_HEAD.name} />
+        </div>
+      </section>
+      <section>{RESUME_HEAD.description}</section>
+      <section>hi</section>
+      <section>hi</section>
+    </main>
+  );
 }
---
+++ constants/resume.ts
@@ -0,0 +1,5 @@
+export const RESUME_HEAD = {
+  name: "이교헌 (Lee GyoHeon)",
+  description: "안녕하세요! 프론트엔드 개발자 이교헌입니다.",
+  image: "/image/lgh.png",
+};

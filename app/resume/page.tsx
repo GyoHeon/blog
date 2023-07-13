b9647620c5fdf51fd7a78a913887a5e960f51a6b
GyoHeonLee <ghl12078@gmail.com>
2023-07-13 21:11:05 +0900

    Feat: Add resume information

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,16 +1,35 @@
 import { RESUME_HEAD } from "@/constants/resume";
 import Image from "next/image";
 
+function HeadInfo() {
+  return (
+    <ul className="flex flex-col mt-10 md:mt-0 border-b-[1px] border-white">
+      {RESUME_HEAD.information.map((info) => (
+        <li key={info.title} className="flex gap-4 items-center py-1 border-t-[1px] border-white">
+          <h6 className="w-32 md:w-20 text-end text-[--blue]">{info.title}</h6>
+          <a>{info.value}</a>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
 export default function Home() {
   return (
-    <main className="flex flex-col gap-12 w-full">
+    <main className="flex flex-col gap-12 md:w-96">
       <section>
-        <h2 className="text-6xl font-bold">{RESUME_HEAD.name}</h2>
-        <div>
+        <h2 className="title-resume text-4xl font-bold">{RESUME_HEAD.name}</h2>
+        <div className="flex flex-col md:flex-row justify-between">
           <Image src={RESUME_HEAD.image} width={300} height={300} alt={RESUME_HEAD.name} />
+          <HeadInfo />
         </div>
       </section>
-      <section>{RESUME_HEAD.description}</section>
+
+      <section>
+        <h2 className="title-resume">About Me</h2>
+
+        <p>{RESUME_HEAD.description}</p>
+      </section>
       <section>hi</section>
       <section>hi</section>
     </main>
--- constants/resume.ts
+++ constants/resume.ts
@@ -1,5 +1,12 @@
 export const RESUME_HEAD = {
-  name: "이교헌 (Lee GyoHeon)",
-  description: "안녕하세요! 프론트엔드 개발자 이교헌입니다.",
+  name: "이교헌",
+  description: "안녕하세요! 프론트엔드 개발자 이교헌입니다. kk",
   image: "/image/lgh.png",
+  information: [
+    { title: "Phone", value: "010-9978-9874" },
+    { title: "Email", value: "ghl12078@gmail.com" },
+    { title: "GitHub", value: "https://github.com/GyoHeon" },
+    { title: "Blog", value: "https://gyoheon.com" },
+    { title: "Portfolio", value: "https://gyoheon.com" },
+  ],
 };

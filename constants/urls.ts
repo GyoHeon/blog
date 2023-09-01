992bf80a13638cb1451691aec751f662e9ae6707
GyoHeonLee <ghl12078@gmail.com>
2023-09-02 04:14:11 +0900

    Feat: Delete portfolio page

--- constants/urls.ts
+++ constants/urls.ts
@@ -1,6 +1,5 @@
 export const HEADER_MENUS = [
   { name: "Posts", url: "/posts" },
   { name: "Resume", url: "/resume" },
-  { name: "Portfolio", url: "/portfolio" },
   { name: "Memos", url: "/memos" },
 ] as const;

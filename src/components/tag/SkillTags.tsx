08549901de9494bd223b64577cca4b6813522c75
Gyoheon <ghl12078@gmail.com>
2023-09-22 09:05:37 +0900

    Fix: Fix tag flex error

--- src/components/tag/SkillTags.tsx
+++ src/components/tag/SkillTags.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export function SkillTags({ stack }: Props) {
   return (
-    <ul className="flex gap-2 my-2">
+    <ul className="flex flex-wrap gap-2 my-2">
       {stack.map((stack) => (
         <li key={stack} className="tag">
           {stack}

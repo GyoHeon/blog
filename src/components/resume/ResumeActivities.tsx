b5a91eef74e694386372af825682dc66ce4df4b8
GyoHeonLee <ghl12078@gmail.com>
2023-10-08 15:44:00 +0900

    Fix: Fix error

--- src/components/resume/ResumeActivities.tsx
+++ src/components/resume/ResumeActivities.tsx
@@ -7,11 +7,10 @@ export function ResumeActivities({ title, projects }: IResumeActivities) {
   return (
     <ResumeSection title={title}>
       <ul>
-        {projects.map(({ title, date, description, values, link = "" }) => (
+        {projects.map(({ title, date, values, link = "" }) => (
           <li key={title} className="flex flex-col gap-2 pb-6 border-b border-neutral-700">
             <ProjectHeader title={title} link={link} />
             <Date date={date} />
-            {description && <span>{description}</span>}
             <ActionList values={values} />
           </li>
         ))}

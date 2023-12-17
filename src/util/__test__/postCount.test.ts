7bf1ae7cad05ae30683d3e77614a015ae15a3634
GyoHeonLee <ghl12078@gmail.com>
2023-12-17 23:12:27 +0900

    Feat: Add vitest

--- src/util/__test__/postCount.test.ts
+++ src/util/__test__/postCount.test.ts
@@ -1,15 +1,21 @@
-import { afterEach, beforeEach, describe, expect, it } from "jest";
-import { mock } from "mock-fs";
-import { postCount } from "../postCount";
+import mock from "mock-fs";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import { postCount, rootDirectory } from "../postCount";
 
 describe("read posts", () => {
   beforeEach(() => {
     mock({
-      mdx: {
-        post: {
+      [rootDirectory]: {
+        posts: {
           "post1.mdx": "post1",
           "post2.mdx": "post2",
           "post3.mdx": "post3",
+          "post4.mdx": "post4",
+          "post5.mdx": "post5",
+          "post6.mdx": "post6",
+          "post7.mdx": "post7",
+          "post8.mdx": "post8",
+          "post9.mdx": "post9",
         },
       },
     });
@@ -20,6 +26,9 @@ describe("read posts", () => {
   });
 
   it("Should read files", () => {
-    expect(postCount("posts", 6)).toBe(1);
+    expect(postCount("posts", 9)).toBe(1);
+    expect(postCount("posts")).toBe(2);
+    expect(postCount("posts", 3)).toBe(3);
+    expect(postCount("posts", 1)).toBe(9);
   });
 });
--- src/util/postCount.ts
+++ src/util/postCount.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import { isMdx } from "./mdx";
 
-const rootDirectory = path.join(process.cwd(), "mdx");
+export const rootDirectory = path.join(process.cwd(), "mdx");
 
 export function postCount(postType: TPost, postPerPage = 6) {
   const fileDirectory = path.join(rootDirectory, postType);

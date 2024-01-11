e4f82a381ed82e30f7a5fec2c14fd2d8c603ed14
GyoHeonLee <ghl12078@gmail.com>
2024-01-12 00:40:13 +0900

    Chore: Edit style

--- src/components/post/PostHeader.tsx
+++ src/components/post/PostHeader.tsx
@@ -8,7 +8,7 @@ export function PostHeader({ title, date, tags }: Props) {
     <header className="flex flex-col mb-5">
       <Tags tags={tags} />
       <h1 className="py-2 border-b-2 border-[--blue]">{title}</h1>
-      <Date className="border-2 border-t-0 border-[--blue]" date={date} />
+      <Date className="px-1 border-2 border-t-0 border-[--blue]" date={date} />
     </header>
   );
 }
